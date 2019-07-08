17125f72482d2e1165cb23288899de387e32f860
vggithub7 <51913783+vggithub7@users.noreply.github.com>
2019-07-08 10:45:26 +0530

    added new file called day25RunningTimeAndComplexity

---
+++ Day25RunningTimeAndComplexity.py
@@ -0,0 +1,17 @@
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+for j in range(int(raw_input())):
+    flag=0
+    num=int(raw_input())
+    for i in range(2,int(math.sqrt(num))+1):
+        #print "i=",i,"num=",num
+        if num%i==0:
+            flag=1
+#while(range):
+    if num==1:
+        print "Not prime"
+    elif flag==0:
+        print "Prime"
+    else:
+        print "Not prime"     
